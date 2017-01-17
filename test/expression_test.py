794601beeadcbd5ef73e7cc2a1c589c16ddaedc7
Tom Ritchford <tom@swirly.com>
2017-01-17 19:23:55 +0100

    More tests.

--- test/expression_test.py
+++ test/expression_test.py
@@ -3,23 +3,28 @@ from expressy.expression import expression
 
 
 class ExpressionTest(unittest.TestCase):
+    def assertEval(self, s):
+        self.assertEqual(expression(s)(), eval(s))
+
     def test_empty(self):
         with self.assertRaises(ValueError):
             expression('')
 
     def test_named_constant(self):
-        self.assertEqual(expression('True')(), True)
-        self.assertEqual(expression('False')(), False)
-        self.assertEqual(expression('None')(), None)
+        self.assertEval('True')
+        self.assertEval('False')
+        self.assertEval('None')
 
     def test_simple_literals(self):
-        self.assertEqual(expression('5')(), 5)
-        self.assertEqual(expression('"False"')(), 'False')
+        self.assertEval('5')
+        self.assertEval('"False"')
 
     def test_collection(self):
-        self.assertEqual(expression('[1, 2, 3]')(), [1, 2, 3])
-        self.assertEqual(expression('(1, 2, 3)')(), (1, 2, 3))
-        self.assertEqual(expression('{1, 2, 3}')(), {1, 2, 3})
+        self.assertEval('[1, 2, 3]')
+        self.assertEval('(1, 2, 3)')
+        self.assertEval('{1, 2, 3}')
 
     def test_dict(self):
-        self.assertEqual(expression('{}')(), {})
+        self.assertEval('{}')
+        self.assertEval('{1: 2}')
+        self.assertEval('{1: 2, 2:{3: 4, 5: {}}}')
