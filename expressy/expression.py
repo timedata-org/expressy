d69e249d3044baba1d69c31115352eaccff1a6f4
Tom Ritchford <tom@swirly.com>
2017-01-18 14:21:26 +0100

    First try at make_constant.

--- expressy/expression.py
+++ expressy/expression.py
@@ -5,14 +5,16 @@ NO_SYMBOLS = {}.__getitem__
 
 
 class Expression(object):
-    def __init__(self, executor, dependent_expressions):
+    def __init__(self, executor, *dependents):
         self.executor = executor
-        self.dependent_expressions = dependent_expressions
+        self.dependents = dependents
 
     def __call__(self, symbols=NO_SYMBOLS):
-        evaluated = [e(symbols) for e in self.dependent_expressions]
+        evaluated = [e(symbols) for e in self.dependents]
         v = self.executor(*evaluated)
-        return symbols(v.value) if isinstance(v, value.Symbol) else v
+        if isinstance(v, value.Symbol):
+            return symbols(v.value)
+        return v
 
 
 def make_expression(node):
@@ -20,9 +22,8 @@ def make_expression(node):
     assert callable(executor), str(type(executor))
 
     # Recursive call to the constructor here!
-    dependent_expressions = (make_expression(n) for n in dependent_nodes)
-
-    return Expression(executor, tuple(dependent_expressions))
+    dependents = (make_expression(n) for n in dependent_nodes)
+    return Expression(executor, *dependents)
 
 
 def expression(s):
@@ -33,6 +34,26 @@ def expression(s):
     return make_expression(module.body[0])
 
 
-def evaluate_constant(expression, symbols, variables):
-    """Evaluate every part that isn't a variable symbol or dependent on it."""
-    pass
+def evaluate_constant(expression, is_variable, symbols=NO_SYMBOLS):
+    """Recursively evaluate every part that isn't a variable symbol or
+    dependent on one.
+    """
+
+    def make_constant(expr):
+        if isinstance(expr.executor, value.Symbol):
+            symbol_name = expr.executor.value
+            if is_variable(symbol_name):
+                return Expression(value.Value(result))
+
+        else:
+            dependents = [make_constant(d) for d in expr.dependents]
+            if any(isinstance(d, Expression) for d in dependents):
+                for i, d in dependents:
+                    if not isinstance(d, Expression):
+                        dependents[i] = Expression(value.Value(d))
+
+                return Expression(expr.executor, *dependents)
+
+        return expr(symbols)
+
+    return make_constant(expression)
